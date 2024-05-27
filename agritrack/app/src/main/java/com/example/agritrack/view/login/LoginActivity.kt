acb8a740d67ad2c5ff2fe5a9c0204840285bb67d
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-05-27 14:51:38 +0800

    feat: register activity with fragment

--- agritrack/app/src/main/java/com/example/agritrack/view/login/LoginActivity.kt
+++ agritrack/app/src/main/java/com/example/agritrack/view/login/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.example.agritrack.view.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.agritrack.R
 import com.example.agritrack.databinding.ActivityLoginBinding
+import com.example.agritrack.view.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -21,6 +24,10 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.tvRegister.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
 }
---
+++ agritrack/app/src/main/java/com/example/agritrack/view/register/RegisterActivity.kt
@@ -0,0 +1,37 @@
+package com.example.agritrack.view.register
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import com.example.agritrack.R
+import com.example.agritrack.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.frame_container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val fragmentManager = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment) {
+            fragmentManager.commit {
+                add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
+            }
+        }
+    }
+}

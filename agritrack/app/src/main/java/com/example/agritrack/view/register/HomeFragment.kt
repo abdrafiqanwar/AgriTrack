476635d93355b3bb0dc899fba778d17f981f6577
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-05-28 13:10:54 +0800

    feat: register fragment

--- agritrack/app/src/main/java/com/example/agritrack/view/register/HomeFragment.kt
+++ agritrack/app/src/main/java/com/example/agritrack/view/register/HomeFragment.kt
@@ -5,18 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
 import com.example.agritrack.R
+import com.example.agritrack.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvLogin.setOnClickListener{ requireActivity().finish() }
+
+        val fragmentManager = parentFragmentManager
+        val registerFragment = RegisterFragment()
+
+        val bundle = Bundle()
+
+        binding.btnOwner.setOnClickListener{
+            bundle.putString("user", "Business Owner")
+            registerFragment.arguments = bundle
+            fragmentManager.commit {
+                addToBackStack(null)
+                replace(R.id.frame_container, registerFragment, RegisterFragment::class.java.simpleName)
+            }
+        }
+
+        binding.btnConsumer.setOnClickListener{
+            bundle.putString("user", "Consumer")
+            registerFragment.arguments = bundle
+            fragmentManager.commit {
+                addToBackStack(null)
+                replace(R.id.frame_container, registerFragment, RegisterFragment::class.java.simpleName)
+            }
+        }
     }
 }
---
+++ agritrack/app/src/main/java/com/example/agritrack/view/register/RegisterFragment.kt
@@ -0,0 +1,42 @@
+package com.example.agritrack.view.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.commit
+import com.example.agritrack.R
+import com.example.agritrack.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentManager = parentFragmentManager
+        val homeFragment = HomeFragment()
+
+        binding.btnBack.setOnClickListener{
+            fragmentManager.commit {
+                replace(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
+            }
+        }
+
+        if (arguments != null) {
+            val user = arguments?.getString("user")
+            binding.tvUser.text = user
+        }
+    }
+}
